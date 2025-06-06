8b4c08895e2b09cea789a7f1045e9e2991359d21
David <david@martinengeanne.nl>
2025-06-06 15:03:53 +0200

    Some changes to vector2D

--- Vector2D/vector2D.cpp
+++ Vector2D/vector2D.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+class Vector2D {
+    public:
+        int x;
+        int y;
+
+        static Vector2D newVector2D(int x, int y) {
+            return Vector2D { x, y };
+        }
+
+        std::string toString() {
+            std::string vectorToString = std::format("x: {}, y: {}", x, y);
+            return vectorToString;
+        }
+};
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Vector2D vec = Vector2D::newVector2D(3, 7);
+    std::cout << vec.toString() << '\n';
     return 0;
 }
